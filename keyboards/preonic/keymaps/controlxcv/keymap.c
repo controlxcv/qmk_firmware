d305ea5a2794acf65c32353dc53ae41a3c460676
Jonathan Sahagun <66657471+controlxcv@users.noreply.github.com>
2020-09-21 18:22:34 -0500

    Revert "Revert "Factor out keys common to the 3 layouts""
    This reverts commit c92213e977783d48484ff37900b93b5757507b1c.

--- keyboards/preonic/keymaps/controlxcv/keymap.c
+++ keyboards/preonic/keymaps/controlxcv/keymap.c
@@ -6,6 +6,7 @@ enum preonic_layers {
     _QWERTY,
     _COLEMAK,
     _DVORAK,
+    _COMMON,
     _LOWER,
     _RAISE,
     _ADJUST
@@ -23,28 +24,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = KEYMAP_QWERTY,
     [_COLEMAK] = KEYMAP_COLEMAK,
     [_DVORAK] = KEYMAP_DVORAK,
+    [_COMMON] = KEYMAP_COMMON,
     [_LOWER] = KEYMAP_LOWER,
     [_RAISE] = KEYMAP_RAISE,
     [_ADJUST] = KEYMAP_ADJUST
 };
 
+void use_default_layer(uint8_t default_layer) {
+#if defined(AUDIO_ENABLE) && defined(DEFAULT_LAYER_SONGS)
+    float default_layer_songs[][16][2] = DEFAULT_LAYER_SONGS;
+    PLAY_SONG(default_layer_songs[default_layer]);
+#endif
+    default_layer_set((1U << _COMMON) | (1U << default_layer));
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case QWERTY:
             if (record->event.pressed) {
-                set_single_persistent_default_layer(_QWERTY);
+                use_default_layer(_QWERTY);
             }
             return false;
             break;
         case COLEMAK:
             if (record->event.pressed) {
-                set_single_persistent_default_layer(_COLEMAK);
+                use_default_layer(_COLEMAK);
             }
             return false;
             break;
         case DVORAK:
             if (record->event.pressed) {
-                set_single_persistent_default_layer(_DVORAK);
+                use_default_layer(_DVORAK);
             }
             return false;
             break;
