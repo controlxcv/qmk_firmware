f42a59726da1f039bfe5e94bd844eaf0d014d89a
Jonathan Sahagun <66657471+controlxcv@users.noreply.github.com>
2020-09-22 02:53:02 -0500

    Revert to using horizontal layout declarations

--- keyboards/preonic/keymaps/controlxcv/_defs.h
+++ keyboards/preonic/keymaps/controlxcv/_defs.h
@@ -4,9 +4,6 @@
     extern float default_layer_songs[][16][2];
 #endif
 
-/* Make KC_NO easier on the eyes */
-#define ___X___ XXXXXXX
-
 /*
     This macro produces the same output as LAYOUT_ortho_5x12;
     it's just that it's written in code as if the keyboard were
@@ -15,6 +12,7 @@
 
     Note that this is rotation and not transposition.
 */
+/*
 #define LAYOUT_ortho_5x12_vertical( \
     k40, k30, k20, k10, k00, \
     k41, k31, k21, k11, k01, \
@@ -41,3 +39,4 @@
     { k46, k47, k48, k43, k44, k45 } \
 }
 #define LAYOUT_preonic_grid_vertical LAYOUT_ortho_5x12_vertical
+*/
--- keyboards/preonic/keymaps/controlxcv/_layers.h
+++ keyboards/preonic/keymaps/controlxcv/_layers.h
@@ -1,121 +1,65 @@
 #pragma once
 
-#define KEYMAP_QWERTY LAYOUT_preonic_grid_vertical( \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    KC_Z,       KC_A,       KC_Q,       XXXXXXX,    \
-    XXXXXXX,    KC_X,       KC_S,       KC_W,       XXXXXXX,    \
-    XXXXXXX,    KC_C,       KC_D,       KC_E,       XXXXXXX,    \
-    XXXXXXX,    KC_V,       KC_F,       KC_R,       XXXXXXX,    \
-    XXXXXXX,    KC_B,       KC_G,       KC_T,       XXXXXXX,    \
-    XXXXXXX,    KC_N,       KC_H,       KC_Y,       XXXXXXX,    \
-    XXXXXXX,    KC_M,       KC_J,       KC_U,       XXXXXXX,    \
-    XXXXXXX,    KC_COMM,    KC_K,       KC_I,       XXXXXXX,    \
-    XXXXXXX,    KC_DOT,     KC_L,       KC_O,       XXXXXXX,    \
-    XXXXXXX,    KC_SLSH,    KC_SCLN,    KC_P,       XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX     \
+#define KEYMAP_QWERTY LAYOUT_preonic_grid( \
+	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	\
+	XXXXXXX,	KC_Q,		KC_W,		KC_E,		KC_R,		KC_T,		KC_Y,		KC_U,		KC_I,		KC_O,		KC_P,		XXXXXXX,	\
+	XXXXXXX,	KC_A,		KC_S,		KC_D,		KC_F,		KC_G,		KC_H,		KC_J,		KC_K,		KC_L,		KC_SCLN,	XXXXXXX,	\
+	XXXXXXX,	KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,		KC_N,		KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	XXXXXXX,	\
+	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX		\
 )
 
-#define KEYMAP_COLEMAK LAYOUT_preonic_grid_vertical( \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    KC_Z,       KC_A,       KC_Q,       XXXXXXX,    \
-    XXXXXXX,    KC_X,       KC_R,       KC_W,       XXXXXXX,    \
-    XXXXXXX,    KC_C,       KC_S,       KC_F,       XXXXXXX,    \
-    XXXXXXX,    KC_V,       KC_T,       KC_P,       XXXXXXX,    \
-    XXXXXXX,    KC_B,       KC_D,       KC_G,       XXXXXXX,    \
-    XXXXXXX,    KC_K,       KC_H,       KC_J,       XXXXXXX,    \
-    XXXXXXX,    KC_M,       KC_N,       KC_L,       XXXXXXX,    \
-    XXXXXXX,    KC_COMM,    KC_E,       KC_U,       XXXXXXX,    \
-    XXXXXXX,    KC_DOT,     KC_I,       KC_Y,       XXXXXXX,    \
-    XXXXXXX,    KC_SLSH,    KC_O,       KC_SCLN,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX     \
+#define KEYMAP_COLEMAK LAYOUT_preonic_grid( \
+	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	\
+	XXXXXXX,	KC_Q,		KC_W,		KC_F,		KC_P,		KC_G,		KC_J,		KC_L,		KC_U,		KC_Y,		KC_SCLN,	XXXXXXX,	\
+	XXXXXXX,	KC_A,		KC_R,		KC_S,		KC_T,		KC_D,		KC_H,		KC_N,		KC_E,		KC_I,		KC_O,		XXXXXXX,	\
+	XXXXXXX,	KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,		KC_K,		KC_M,		KC_COMM,	KC_DOT,		KC_SLSH,	XXXXXXX,	\
+	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX		\
 )
 
-#define KEYMAP_DVORAK LAYOUT_preonic_grid_vertical( \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    KC_SCLN,    KC_A,       KC_SLSH,    XXXXXXX,    \
-    XXXXXXX,    KC_Q,       KC_O,       KC_COMM,    XXXXXXX,    \
-    XXXXXXX,    KC_J,       KC_E,       KC_DOT,     XXXXXXX,    \
-    XXXXXXX,    KC_K,       KC_U,       KC_P,       XXXXXXX,    \
-    XXXXXXX,    KC_X,       KC_I,       KC_Y,       XXXXXXX,    \
-    XXXXXXX,    KC_B,       KC_D,       KC_F,       XXXXXXX,    \
-    XXXXXXX,    KC_M,       KC_H,       KC_G,       XXXXXXX,    \
-    XXXXXXX,    KC_W,       KC_T,       KC_C,       XXXXXXX,    \
-    XXXXXXX,    KC_V,       KC_N,       KC_R,       XXXXXXX,    \
-    XXXXXXX,    KC_Z,       KC_S,       KC_L,       XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX     \
+#define KEYMAP_DVORAK LAYOUT_preonic_grid( \
+	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	\
+	XXXXXXX,	KC_SLSH,	KC_COMM,	KC_DOT,		KC_P,		KC_Y,		KC_F,		KC_G,		KC_C,		KC_R,		KC_L,		XXXXXXX,	\
+	XXXXXXX,	KC_A,		KC_O,		KC_E,		KC_U,		KC_I,		KC_D,		KC_H,		KC_T,		KC_N,		KC_S,		XXXXXXX,	\
+	XXXXXXX,	KC_SCLN,	KC_Q,		KC_J,		KC_K,		KC_X,		KC_B,		KC_M,		KC_W,		KC_V,		KC_Z,		XXXXXXX,	\
+	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX		\
 )
 
-#define KEYMAP_COMMON LAYOUT_preonic_grid_vertical( \
-    KC_LCTL,    KC_TAB,     KC_QUOT,    KC_BSLS,    KC_GRV,     \
-    KC_LALT,    _______,    _______,    _______,    KC_1,       \
-    LOWER,      _______,    _______,    _______,    KC_2,       \
-    RAISE,      _______,    _______,    _______,    KC_3,       \
-    KC_LGUI,    _______,    _______,    _______,    KC_4,       \
-    KC_SPC,     _______,    _______,    _______,    KC_5,       \
-    KC_SPC,     _______,    _______,    _______,    KC_6,       \
-    KC_LSFT,    _______,    _______,    _______,    KC_7,       \
-    KC_LEFT,    _______,    _______,    _______,    KC_8,       \
-    KC_DOWN,    _______,    _______,    _______,    KC_9,       \
-    KC_UP,      _______,    _______,    _______,    KC_0,       \
-    KC_RGHT,    KC_ENT,     KC_MINS,    KC_EQL,     KC_BSPC     \
+#define KEYMAP_COMMON LAYOUT_preonic_grid( \
+	KC_GRV,		KC_1,		KC_2,		KC_3,		KC_4,		KC_5,		KC_6,		KC_7,		KC_8,		KC_9,		KC_0,		KC_BSPC,	\
+	KC_BSLS,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	KC_EQL,		\
+	KC_QUOT,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	KC_MINS,	\
+	KC_TAB,		_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	KC_ENT,		\
+	KC_LCTL,	KC_LALT,	LOWER,		RAISE,		KC_LGUI,	KC_SPC,		KC_SPC,		KC_LSFT,	KC_LEFT,	KC_DOWN,	KC_UP,		KC_RGHT		\
 )
 
-#define KEYMAP_RAISE LAYOUT_preonic_grid_vertical( \
-    _______,    _______,    _______,    _______,    KC_ESC,     \
-    _______,    _______,    _______,    KC_F11,     KC_F1,      \
-    _______,    _______,    _______,    KC_F12,     KC_F2,      \
-    _______,    _______,    _______,    KC_F13,     KC_F3,      \
-    _______,    _______,    _______,    KC_F14,     KC_F4,      \
-    _______,    _______,    _______,    KC_F15,     KC_F5,      \
-    _______,    _______,    _______,    KC_F16,     KC_F6,      \
-    _______,    _______,    _______,    KC_F17,     KC_F7,      \
-    KC_HOME,    KC_LBRC,    _______,    KC_F18,     KC_F8,      \
-    KC_PGDN,    KC_RBRC,    _______,    KC_F19,     KC_F9,      \
-    KC_PGUP,    _______,    _______,    _______,    KC_F10,     \
-    KC_END,     KC_PENT,    KC_LANG1,   KC_LANG2,   KC_DEL      \
+#define KEYMAP_RAISE LAYOUT_preonic_grid( \
+	KC_ESC,		KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,		KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_DEL,		\
+	_______,	KC_F11,		KC_F12,		KC_F13,		KC_F14,		KC_F15,		KC_F16,		KC_F17,		KC_F18,		KC_F19,		_______,	KC_LANG2,	\
+	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	KC_LANG1,	\
+	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	KC_LBRC,	KC_RBRC,	_______,	KC_PENT,	\
+	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	KC_HOME,	KC_PGDN,	KC_PGUP,	KC_END		\
 )
 
-#define KEYMAP_LOWER LAYOUT_preonic_grid_vertical( \
-    _______,    _______,    _______,    _______,    _______,    \
-    _______,    _______,    _______,    _______,    _______,    \
-    _______,    _______,    _______,    _______,    _______,    \
-    _______,    _______,    _______,    _______,    _______,    \
-    _______,    _______,    _______,    _______,    _______,    \
-    _______,    _______,    _______,    _______,    _______,    \
-    _______,    _______,    _______,    _______,    _______,    \
-    _______,    _______,    _______,    _______,    _______,    \
-    KC_MPRV,    _______,    _______,    _______,    _______,    \
-    KC_VOLD,    _______,    _______,    _______,    _______,    \
-    KC_VOLU,    _______,    _______,    _______,    _______,    \
-    KC_MNXT,    KC_MPLY,    _______,    _______,    _______     \
+#define KEYMAP_LOWER LAYOUT_preonic_grid( \
+	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	\
+	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	\
+	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	\
+	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	KC_MPLY,	\
+	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	KC_MPRV,	KC_VOLD,	KC_VOLU,	KC_MNXT		\
 )
 
-#define KEYMAP_ADJUST LAYOUT_preonic_grid_vertical( \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    CK_TOGG,    QWERTY,     XXXXXXX,    \
-    _______,    XXXXXXX,    XXXXXXX,    COLEMAK,    XXXXXXX,    \
-    _______,    XXXXXXX,    XXXXXXX,    DVORAK,     XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    MO(_RESET)  \
+#define KEYMAP_ADJUST LAYOUT_preonic_grid( \
+	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	MO(_RESET),	\
+	XXXXXXX,	QWERTY,		COLEMAK,	DVORAK,		XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	\
+	XXXXXXX,	CK_TOGG,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	\
+	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	\
+	XXXXXXX,	XXXXXXX,	_______,	_______,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX		\
 )
 
-#define KEYMAP_RESET LAYOUT_preonic_grid_vertical( \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    RESET,      \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    _______,    XXXXXXX,    XXXXXXX,    EEP_RST,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _______     \
+#define KEYMAP_RESET LAYOUT_preonic_grid( \
+	RESET,		XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	_______,	\
+	XXXXXXX,	XXXXXXX,	XXXXXXX,	EEP_RST,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	\
+	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	\
+	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	\
+	XXXXXXX,	XXXXXXX,	_______,	_______,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX		\
 )
