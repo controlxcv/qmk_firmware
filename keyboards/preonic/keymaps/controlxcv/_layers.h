561564c265fc63af18f596eefcf00c15a4b52d9f
Jonathan Sahagun <66657471+controlxcv@users.noreply.github.com>
2020-09-23 02:57:05 -0500

    Changed layout

--- keyboards/preonic/keymaps/controlxcv/_layers.h
+++ keyboards/preonic/keymaps/controlxcv/_layers.h
@@ -26,49 +26,49 @@
 
 #define KEYMAP_COMMON LAYOUT_preonic_grid( \
     KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPC,    \
-    KC_BSLS,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_EQL,     \
-    KC_QUOT,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_MINS,    \
-    KC_LSFT,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_RSFT,    \
-    _KC_FUNC1,  _KC_FUNC2,  KC_LCTL,    KC_LALT,    KC_LGUI,    KC_SPC,     KC_SPC,     KC_ENT,     KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT     \
+    KC_TAB,     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_EQL,     \
+    KC_BSLS,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_MINS,    \
+    KC_LSFT,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_QUOT,    \
+    _KC_LOWER,  KC_LCTL,    KC_LALT,    _KC_RAISE,  KC_LGUI,    KC_SPC,     KC_SPC,     KC_ENT,     KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT     \
 )
 
-#define KEYMAP_FUNC1 LAYOUT_preonic_grid( \
-    KC_ESC,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_EJCT,    \
+#define KEYMAP_LOWER LAYOUT_preonic_grid( \
+    TG(_LOCK),  KC_F11,     KC_F12,     KC_F13,     KC_F14,     KC_F15,     KC_F16,     KC_F17,     KC_F18,     KC_F19,     KC_MUTE,    KC_EJCT,    \
+    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_LANG2,   \
+    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_LANG1,   \
     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    \
+    _______,    _______,    _______,    _______,    _______,    KC_MPLY,    KC_MPLY,    KC_MSTP,    KC_MPRV,    KC_VOLU,    KC_VOLD,    KC_MNXT     \
+)
+
+#define KEYMAP_RAISE LAYOUT_preonic_grid( \
+    KC_ESC,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_DEL,     \
+    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_LBRC,    \
+    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_RBRC,    \
     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    \
-    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    \
-    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_BSPC,    KC_LEFT,    KC_RGHT,    KC_DEL      \
+    _______,    _______,    _______,    _______,    _______,    KC_BSPC,    KC_BSPC,    KC_DEL,     KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END      \
 )
 //G(KC_Z),    G(KC_X),    G(KC_C),    G(KC_V),    
 
-#define KEYMAP_FUNC2 LAYOUT_preonic_grid( \
-    KC_ESC,     KC_F11,     KC_F12,     KC_PSCR,    KC_SLCK,    KC_PAUS,    _______,    _______,    _______,    KC_LBRC,    KC_RBRC,    KC_DEL,     \
-    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_LANG2,   \
-    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_LANG1,   \
-    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_CAPS,    \
-    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_TAB,     KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END      \
-)
-
 #define KEYMAP_CONFIG LAYOUT_preonic_grid( \
-    MO(_RESET), XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    _KC_QWERTY, _KC_COLMAK, _KC_DVORAK, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    CK_TOGG,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    TG(_LOCK),  \
-    _______,    _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_MPLY,    KC_MPRV,    KC_VOLD,    KC_VOLU,    KC_MNXT     \
+    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    CK_TOGG,    _KC_QWERTY, \
+    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _KC_COLMAK, \
+    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _KC_DVORAK, \
+    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
+    _______,    XXXXXXX,    XXXXXXX,    _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    MO(_RESET), XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX     \
 )
 
 #define KEYMAP_LOCK LAYOUT_preonic_grid( \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    TG(_LOCK),  \
+    TG(_LOCK),  XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    _______,    _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX     \
+    _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX     \
 )
 
 #define KEYMAP_RESET LAYOUT_preonic_grid( \
-    _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    RESET,      \
+    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    RESET,      \
     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    \
-    _______,    _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    EEP_RST,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX     \
+    _______,    XXXXXXX,    XXXXXXX,    _______,    XXXXXXX,    EEP_RST,    EEP_RST,    _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX     \
 )
--- keyboards/preonic/keymaps/controlxcv/keymap.c
+++ keyboards/preonic/keymaps/controlxcv/keymap.c
@@ -8,8 +8,8 @@ enum preonic_layers {
     _COLEMAK,
     _DVORAK,
     _COMMON,
-    _FUNC1 = 8,
-    _FUNC2,
+    _LOWER = 8,
+    _RAISE,
     _LOCK = 13,
     _CONFIG,
     _RESET
@@ -19,8 +19,8 @@ enum preonic_keycodes {
     _KC_QWERTY = SAFE_RANGE,
     _KC_COLMAK,
     _KC_DVORAK,
-    _KC_FUNC1,
-    _KC_FUNC2
+    _KC_LOWER,
+    _KC_RAISE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -28,8 +28,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = KEYMAP_COLEMAK,
     [_DVORAK] = KEYMAP_DVORAK,
     [_COMMON] = KEYMAP_COMMON,
-    [_FUNC1] = KEYMAP_FUNC1,
-    [_FUNC2] = KEYMAP_FUNC2,
+    [_LOWER] = KEYMAP_LOWER,
+    [_RAISE] = KEYMAP_RAISE,
     [_LOCK] = KEYMAP_LOCK,
     [_CONFIG] = KEYMAP_CONFIG,
     [_RESET] = KEYMAP_RESET
@@ -38,11 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Local functions */
 
 void use_default_layer(uint8_t default_layer) {
-#if defined(AUDIO_ENABLE) && defined(DEFAULT_LAYER_SONGS)
-    PLAY_SONG(default_layer_songs[default_layer]);
-#endif
     default_layer_set(1U << default_layer);
-    //layer_on(_COMMON);
 }
 
 /* QMK functions */
@@ -54,19 +50,23 @@ void keyboard_post_init_user(void) {
     clicky_off();
 }
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-
-    // if (layer_state_cmp(state, _COMMON)) {
-    //     PLAY_SONG(default_layer_songs[_QWERTY]);
-    // }
-
+layer_state_t default_layer_state_set_user(layer_state_t state) {
 #if defined(AUDIO_ENABLE) && defined(DEFAULT_LAYER_SONGS)
     /* Play a tune when a keyboard layout is activated */
-    // if (layer_state_cmp(state, _QWERTY)) {
-    //     PLAY_SONG(default_layer_songs[_QWERTY]);
-    // }
+    if (layer_state_cmp(state, _QWERTY)) {
+        PLAY_SONG(default_layer_songs[_QWERTY]);
+    }
+    else if (layer_state_cmp(state, _COLEMAK)) {
+        PLAY_SONG(default_layer_songs[_COLEMAK]);
+    }
+    else if (layer_state_cmp(state, _DVORAK)) {
+        PLAY_SONG(default_layer_songs[_DVORAK]);
+    }
 #endif
+    return state;
+}
 
+layer_state_t layer_state_set_user(layer_state_t state) {
     return state;
 }
 
@@ -90,23 +90,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
             break;
-        case _KC_FUNC1:
+        case _KC_LOWER:
             if (record->event.pressed) {
-                layer_on(_FUNC1);
-                update_tri_layer(_FUNC1, _FUNC2, _CONFIG);
+                layer_on(_LOWER);
+                update_tri_layer(_LOWER, _RAISE, _CONFIG);
             } else {
-                layer_off(_FUNC1);
-                update_tri_layer(_FUNC1, _FUNC2, _CONFIG);
+                layer_off(_LOWER);
+                update_tri_layer(_LOWER, _RAISE, _CONFIG);
             }
             return false;
             break;
-        case _KC_FUNC2:
+        case _KC_RAISE:
             if (record->event.pressed) {
-                layer_on(_FUNC2);
-                update_tri_layer(_FUNC1, _FUNC2, _CONFIG);
+                layer_on(_RAISE);
+                update_tri_layer(_LOWER, _RAISE, _CONFIG);
             } else {
-                layer_off(_FUNC2);
-                update_tri_layer(_FUNC1, _FUNC2, _CONFIG);
+                layer_off(_RAISE);
+                update_tri_layer(_LOWER, _RAISE, _CONFIG);
             }
             return false;
             break;
