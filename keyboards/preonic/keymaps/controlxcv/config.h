7a398dbf39eca03ab3078b95870b2cc49daf4dde
Jonathan Sahagun <66657471+controlxcv@users.noreply.github.com>
2020-10-05 00:49:10 -0500

    Clean up build options

--- keyboards/preonic/keymaps/controlxcv/config.h
+++ keyboards/preonic/keymaps/controlxcv/config.h
@@ -2,7 +2,6 @@
 
 #ifdef AUDIO_ENABLE
     //#define STARTUP_SONG SONG(PREONIC_SOUND)
-
     #define DEFAULT_LAYER_SONGS { \
         SONG(QWERTY_SOUND), \
         SONG(COLEMAK_SOUND), \
@@ -11,15 +10,4 @@
 #endif
 
 #define NO_MUSIC_MODE
-
-#define AUDIO_CLICKY
-#define AUDIO_CLICKY_DELAY_DURATION 1
-#define AUDIO_CLICKY_FREQ_MIN 220.0f
-#define AUDIO_CLICKY_FREQ_DEFAULT 440.0f
-#define AUDIO_CLICKY_FREQ_MAX 880.0f
-
-// do-re-mi
-#define AUDIO_CLICKY_FREQ_RANDOMNESS 0.2599f
-
-// do-re-mi-fa-so
-// #define AUDIO_CLICKY_FREQ_RANDOMNESS 0.4983f
+#define FORCE_NKRO
