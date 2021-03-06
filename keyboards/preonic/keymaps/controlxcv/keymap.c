df5a9da150641cd19226c1d30b0988053cc1eca5
controlxcv <66657471+controlxcv@users.noreply.github.com>
2021-03-06 02:34:23 -0600

    add some disabled code for planned features

--- keyboards/preonic/keymaps/controlxcv/keymap.c
+++ keyboards/preonic/keymaps/controlxcv/keymap.c
@@ -47,16 +47,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Functions
  */
 
-float eeprom_reset_song[][2] = SONG(STARTUP_SOUND);
+
+#if defined(AUDIO_ENABLE)
+    float eeprom_reset[][2] = SONG(STARTUP_SOUND);
+    // float caps_on[][2] = SONG(CAPS_LOCK_ON_SOUND);
+    // float caps_off[][2] = SONG(CAPS_LOCK_OFF_SOUND);
+    // float num_on[][2] = SONG(NUM_LOCK_ON_SOUND);
+    // float num_off[][2] = SONG(NUM_LOCK_OFF_SOUND);
+    // float scroll_on[][2] = SONG(SCROLL_LOCK_ON_SOUND);
+    // float scroll_off[][2] = SONG(SCROLL_LOCK_OFF_SOUND);
+#endif
+
 void eeconfig_init_user(void) {
     eeconfig_update_rgblight_default();
     rgblight_mode(RGBLIGHT_DEFAULT_MODE);
     default_layer_or(1UL << _COMM);
 
     #if defined(AUDIO_ENABLE)
-        PLAY_SONG(eeprom_reset_song);
+        PLAY_SONG(eeprom_reset);
     #endif /* AUDIO_ENABLE */
 }
+
+// bool led_update_user(led_t led_state) {
+//     #if defined AUDIO_ENABLE
+//         static uint8_t caps_state = 0;
+//         if (caps_state != led_state.caps_lock) {
+//             led_state.caps_lock ? PLAY_SONG(caps_on) : PLAY_SONG(caps_off);
+//             caps_state = led_state.caps_lock;
+//         }
+//         static uint8_t num_state = 0;
+//         if (num_state != led_state.num_lock) {
+//             led_state.num_lock ? PLAY_SONG(num_on) : PLAY_SONG(num_off);
+//             num_state = led_state.num_lock;
+//         }
+//         static uint8_t scroll_state = 0;
+//         if (scroll_state != led_state.scroll_lock) {
+//             led_state.scroll_lock ? PLAY_SONG(scroll_on) : PLAY_SONG(scroll_off);
+//             scroll_state = led_state.scroll_lock;
+//         }
+//     #endif
+//     return true;
+// }
 
 layer_state_t default_layer_state_set_user(layer_state_t state) {
     #if defined(AUDIO_ENABLE) && defined(DEFAULT_LAYER_SONGS)
--- keyboards/preonic/keymaps/controlxcv/layers.h
+++ keyboards/preonic/keymaps/controlxcv/layers.h
@@ -16,22 +16,23 @@
     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,          XXXXXXX,          XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX \
 )
 
-#define KC_SYS_L KC_LGUI
-#define KC_SYS_R KC_LCTL
+#define KC_SYS_1 KC_LALT
+#define KC_SYS_2 KC_LGUI
+#define KC_SYS_3 KC_LCTL
 #define KEYMAP_COMM LAYOUT_preonic_1x2uC( \
     KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPC, \
     KC_TAB,     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_EQL, \
-    KC_QUOT,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_MINS, \
-    KC_LSFT,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_RSFT, \
-    MO(_OVER),  MO(_FUNC),  KC_LALT,    KC_SYS_L,   KC_SYS_R,         KC_SPC,           KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    KC_ENT \
+    KC_BSLS,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_MINS, \
+    KC_LSFT,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_QUOT, \
+    MO(_OVER),  MO(_FUNC),  KC_SYS_1,   KC_SYS_2,   KC_SYS_3,         KC_SPC,           KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    KC_ENT \
 )
 
 #define KEYMAP_OVER LAYOUT_preonic_1x2uC( \
-    KC_LANG1,   KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_DEL, \
-    KC_LANG2,   KC_F11,     KC_F12,     KC_PSCR,    KC_SLCK,    KC_PAUS,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_CLCK, \
-    KC_GRV,     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_NLCK, \
-    _______,    KC_MUTE,    KC_VOLD,    KC_VOLU,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_LBRC,    KC_RBRC,    KC_BSLS,    _______, \
-    _______,    XXXXXXX,    _______,    _______,    _______,          XXXXXXX,          KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     KC_INS \
+    KC_PAUS,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_DEL, \
+    KC_SLCK,    KC_CLCK,    KC_NLCK,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_F11,     KC_F12,     KC_LBRC, \
+    KC_PSCR,    KC_LANG1,   KC_LANG2,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_RBRC, \
+    _______,    KC_MUTE,    KC_VOLD,    KC_VOLU,    KC_MSTP,    KC_MPRV,    KC_MNXT,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_GRV, \
+    _______,    XXXXXXX,    _______,    _______,    _______,          KC_MPLY,          KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     KC_INS \
 )
 
 // Key lock
