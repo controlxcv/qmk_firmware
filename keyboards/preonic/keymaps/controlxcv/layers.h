4e8635ef64e0e6a426470989952be54f31f7e831
controlxcv <66657471+controlxcv@users.noreply.github.com>
2021-03-13 21:22:58 -0600

    update layout

--- keyboards/preonic/keymaps/controlxcv/layers.h
+++ keyboards/preonic/keymaps/controlxcv/layers.h
@@ -31,26 +31,26 @@
     KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPC, \
     KC_BSLS,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_EQL, \
     KC_QUOT,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_MINS, \
-    KC_LSFT,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_SLSH,    KC_UP,      KC_COMM,    KC_DOT, \
-    KC_TAB,     KC_LGUI,    KC_APP,     KC_LALT,    KC_LCTL,          KC_SPC,           MO(_XTND),  KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_ENT \
+    KC_LSFT,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_COMM,    KC_DOT,     KC_SLSH,    KC_ENT, \
+    KC_TAB,     KC_LGUI,    KC_APP,     KC_LALT,    KC_LCTL,          KC_SPC,           MO(_XTND),  KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT \
 )
 
 // Extended keys
 #define KEYMAP_XTND LAYOUT_preonic_1x2uC( \
     KC_PAUS,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_GRV,     KC_LBRC,    KC_RBRC,    KC_DEL, \
-    KC_SLCK,    KC_F5,      KC_F6,      KC_F7,      KC_F8,      XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_VOLU, \
-    KC_PSCR,    KC_F9,      KC_F10,     KC_F11,     KC_F12,     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_VOLD, \
-    _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_PGUP,    KC_LANG1,   KC_LANG2, \
-    KC_CLCK,    _______,    KC_NLCK,    _______,    _______,          MO(_QMKB),        _______,    KC_HOME,    KC_PGDN,    KC_END,     KC_INS \
+    KC_SLCK,    KC_F5,      KC_F6,      KC_F7,      KC_F8,      XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_LANG1, \
+    KC_PSCR,    KC_F9,      KC_F10,     KC_F11,     KC_F12,     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_LANG2, \
+    _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_VOLD,    KC_VOLU,    KC_MUTE,    KC_INS, \
+    KC_CLCK,    _______,    KC_NLCK,    _______,    _______,          MO(_QMKB),        _______,    KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END \
 )
 
 // QMK functions
 #define KEYMAP_QMKB LAYOUT_preonic_1x2uC( \
-    EEP_RST,    DF(_COLE),  DF(_QWER),  XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    RGB_M_P,    RGB_M_R,    RGB_M_SW,   RGB_M_T, \
+    EEP_RST,    DF(_COLE),  DF(_QWER),  XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    RGB_M_P,    RGB_M_SW, \
     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    RGB_MOD, \
     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    RGB_RMOD, \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    RGB_VAI,    RGB_SAD,    RGB_SAI, \
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    MO(_RSET),        _______,          _______,    RGB_HUD,    RGB_VAD,    RGB_HUI,    RGB_TOG \
+    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    RGB_SAD,    RGB_SAI,    RGB_M_T,    RGB_TOG, \
+    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    MO(_RSET),        _______,          _______,    RGB_HUD,    RGB_VAD,    RGB_VAI,    RGB_HUI \
 )
 
 // Reset
