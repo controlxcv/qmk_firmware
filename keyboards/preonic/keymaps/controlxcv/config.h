04ea5e0d0b0489d36b00ab3aa6e35dcea2f20c62
controlxcv <66657471+controlxcv@users.noreply.github.com>
2021-03-23 00:03:59 -0500

    Update config.h

--- keyboards/preonic/keymaps/controlxcv/config.h
+++ keyboards/preonic/keymaps/controlxcv/config.h
@@ -10,34 +10,28 @@
 #define NO_MUSIC_MODE
 #define FORCE_NKRO
 
-/* RGB Layout
-    esc ------------\
-    |  6  5   4  3  |
-    |       0       |
-    |  7  8   1  2  |
-    \----- spc -- ent
-*/
-// #ifdef RGBLED_NUM
-// #undef RGBLED_NUM
-// #endif
-// #define RGBLED_NUM 9
-//                            0  1  2  3  4  5  6  7  8  unmapped
-// #define RGBLIGHT_LED_MAP { 0, 3, 4, 8, 7, 6, 5, 1, 2 }
-// #define RGBLIGHT_LED_MAP { 0, 3, 4, 4, 3, 2, 1, 1, 2 }
-// #define RGBLIGHT_LED_MAP { 2, 2, 3, 4, 3, 2, 1, 0, 1 }
-
 #undef USE_CIE1931_CURVE
 #undef RGBLIGHT_ANIMATIONS
+
 #define RGBLIGHT_EFFECT_RAINBOW_MOOD
 #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 #define RGBLIGHT_EFFECT_RGB_TEST
-#define RGBLIGHT_HUE_STEP 16    // 16 steps, wrap around
-#define RGBLIGHT_DEFAULT_HUE 0
-#define RGBLIGHT_SAT_STEP 15    // 17 steps
+
+/* 16 ticks, wrap around */
+#define RGBLIGHT_HUE_STEP 16
+#define RGBLIGHT_DEFAULT_HUE 16
+
+/* 51 102 153 204 255 */
+#define RGBLIGHT_SAT_STEP 51
 #define RGBLIGHT_DEFAULT_SAT 255
-#define RGBLIGHT_VAL_STEP 15    // 17 steps
-#define RGBLIGHT_DEFAULT_VAL 255
+
+/* 51 102 153 204 255 */
+#define RGBLIGHT_VAL_STEP 51
+#define RGBLIGHT_DEFAULT_VAL 51
+
 #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_SWIRL
 
-//#define CXCV_RGBY_HUE
+/*
+#define CXCV_RGBY_HUE
 #define CXCV_MAC_KEYS
+*/
