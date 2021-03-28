e123ce7b9af43285892e34acfdb4170c5d33ca19
controlxcv <66657471+controlxcv@users.noreply.github.com>
2021-03-28 02:10:22 -0500

    Update lights.c

--- keyboards/preonic/keymaps/controlxcv/lights.c
+++ keyboards/preonic/keymaps/controlxcv/lights.c
@@ -1,8 +1,6 @@
 #include QMK_KEYBOARD_H
 
-/*
-    Multiply range values
-*/
+/* Multiply range values */
 inline uint16_t mult(uint16_t a, uint16_t b) {
     #if UINTMAX_MAX >= UINT32_MAX
         uint32_t result = a * b;
@@ -11,51 +9,35 @@ inline uint16_t mult(uint16_t a, uint16_t b) {
         return (a >> 8) * (b >> 8);
     #endif
 }
-/*
-    16-bit HSV to RGB computation (still outputs 8-bit)
-*/
+/* 16-bit HSV to RGB computation (still outputs 8-bit) */
 RGB rgblight_hsv_to_rgb(HSV hsv) {
 
-    /*
-        Uncomment to bypass
-        
-        return hsv_to_rgb(hsv);
-    */
+    /* return hsv_to_rgb(hsv); */
 
-    /*
-        Zero saturation means it's a shade of gray
-    */
-    if (hsv.s == 0) {
-        return (RGB) { .r = hsv.v, .g = hsv.v, .b = hsv.v };
-    }
+    /* Special cases for zero value and zero saturation respectively */
+    if (hsv.v == 0) { return (RGB) { .r = 0,     .g = 0,     .b = 0     }; }
+    if (hsv.s == 0) { return (RGB) { .r = hsv.v, .g = hsv.v, .b = hsv.v }; }
 
-    /*
-        Stretch precision of input from 8-bit to 16-bit
-    */
+    /* Stretch precision of input from 8-bit to 16-bit */
     uint16_t hue = hsv.h << 8;
     uint16_t sat = hsv.s << 8;
     uint16_t val = hsv.v << 8;
  
-    /*
-        sector  = hue / (65536/6)
-        offset  = 6 * (hue - (sector * (65536/6)))
-        65536/6 = 10922
-    */
-    uint16_t sector = ((hue >> 2) * 3 >> 13);
+    /* sector  = hue / (65536/6)
+       offset  = 6 * (hue - (sector * (65536/6)))
+       65536/6 = 10922                            */
+    #if UINTMAX_MAX >= UINT32_MAX
+        uint16_t sector = (uint32_t)hue * 3 >> 15;
+    #else
+        uint16_t sector = ((hue >> 2) * 3 >> 13);
+    #endif
     uint16_t offset = 6 * (hue - (sector * 10922));
 
-    /*
-        Offset starts from either end of the arc,
-        depending if it's an odd or even sector
-    */
-    if ((sector & 1) == 1) {
-        offset = ~offset;
-    }
+    /* Odd sectors count the offset from the other end */
+    if ((sector & 1) == 1) { offset = ~offset; }
 
-    /*
-        65535 * cos(45)       = 46340
-        65535 * (1 - cos(45)) = 19195;
-    */
+    /* 65535 * cos(45)       = 46340
+       65535 * (1 - cos(45)) = 19195; */
     uint16_t high = mult(val, ~sat + mult(sat, ~mult(offset, 19195))) >> 8;
     uint16_t mid  = mult(val, ~sat + mult(sat,  mult(offset, 46340))) >> 8;
     uint16_t low  = mult(val, ~sat                                  ) >> 8;
