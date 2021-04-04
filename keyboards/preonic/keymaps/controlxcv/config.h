1f96b7d084e961e6f0dcf852b0ca904342bd164e
controlxcv <66657471+controlxcv@users.noreply.github.com>
2021-04-03 22:11:35 -0500

    change light layout

--- keyboards/preonic/keymaps/controlxcv/config.h
+++ keyboards/preonic/keymaps/controlxcv/config.h
@@ -22,7 +22,7 @@
 #undef RGBLIGHT_ANIMATIONS
 
 /*                       |  0  1  2  3  4  5  6  7  8  | */
-#define RGBLIGHT_LED_MAP {  0, 4, 5, 1, 1, 1, 1, 2, 3  }
+#define RGBLIGHT_LED_MAP {  0, 3, 3, 4, 4, 1, 1, 2, 2  }
 
 #define RGBLIGHT_EFFECT_RAINBOW_MOOD
 #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
