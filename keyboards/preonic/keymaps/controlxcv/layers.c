1d4e2c0acc260030861bf77ac587f0c031461b6d
controlxcv <66657471+controlxcv@users.noreply.github.com>
2021-07-18 02:07:10 -0500

    Fix - swap dash and equal

--- keyboards/preonic/keymaps/controlxcv/layers.c
+++ keyboards/preonic/keymaps/controlxcv/layers.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //                 KC_SCLN,    KC_Q,       KC_J,       KC_K,       KC_X,       KC_B,       KC_M,       KC_W,       KC_V,       KC_Z),
 
     [_RAISE] = LAYOUT_preonic_1x2uC(
-        XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_EQL,     KC_MINS,    KC_LBRC,    KC_RBRC,    KC_NLCK,
+        XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_MINS,    KC_EQL,     KC_LBRC,    KC_RBRC,    KC_NLCK,
         XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_CLCK,
         KC_GRV,     KC_IME_ON,  KC_IME_OFF, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_INS,
         _______,    KC_PSCR,    KC_SLCK,    KC_PAUS,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_F9,      KC_F10,     KC_F11,     KC_F12,     XXXXXXX,
